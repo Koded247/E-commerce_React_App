54d7d932abe78e1bf9afceee1503984475aeffbf
abidoye korede <korexcoded@gmail.com>
2024-10-27 13:10:01 +0100

    updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
                         </Link>
                     </p>
                     
-                    {/* media icon  */}
+                   
                     <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                         {/* facebook  */}
                         <a className="text-gray-100 cursor-pointer">
