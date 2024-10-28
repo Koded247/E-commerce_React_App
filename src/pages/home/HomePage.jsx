399dd6d75c4e4615e6e968990b5cf0f02caa39dd
abidoye korede <korexcoded@gmail.com>
2024-10-28 12:43:42 +0100

    updated

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -3,7 +3,7 @@ import HeroSection from "../../components/heroSection/HeroSection";
 import HomePageProductCard from "../../components/homePageProductCard/HomePageProductCard";
 import Layout from "../../components/layout/Layout";
 import Testimonial from "../../components/testimonial/Testimonial";
-import Track from "../../components/track/Track";
+import Track from "../../components/track/Track"
 
 const HomePage = () => {
     return (
