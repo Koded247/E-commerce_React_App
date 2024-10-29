a8c76b31b8101e823e018b9eb1f66b57b4ebdbf6
abidoye korede <korexcoded@gmail.com>
2024-10-29 21:12:46 +0100

    update files

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -16,10 +16,11 @@ const Testimonial = () => {
                         <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
                             <div className="h-full text-center">
                                 <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://ecommerce-sk.vercel.app/img/kamal.png" />
-                                <p className="leading-relaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+                                <p className="leading-relaxed">Shopping on this site was a breeze! It’s user-friendly, and I loved the personalized recommendations.
+                                The checkout process was quick and easy. I appreciated the multiple payment options and secure checkout.</p>
                                 <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
                                 <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">Kamal Nayan Upadhyay</h2>
-                                <p className="text-gray-500">Senior Product Designer</p>
+                                <p className="text-gray-500">Fashion Designer</p>
                             </div>
                         </div>
 
@@ -27,10 +28,11 @@ const Testimonial = () => {
                         <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
                             <div className="h-full text-center">
                                 <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://www.devknus.com/img/gawri.png" />
-                                <p className="leading-relaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+                                <p className="leading-relaxed">For the quality you get, the price is unbeatable. I’m so happy with my purchase!
+                                This is definitely worth the money. It’s high-quality, and you can tell they pay attention to the details</p>
                                 <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
                                 <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">S Mishra</h2>
-                                <p className="text-gray-500">UI Develeoper</p>
+                                <p className="text-gray-500">Engineer</p>
                             </div>
                         </div>
 
@@ -38,10 +40,11 @@ const Testimonial = () => {
                         <div className="lg:w-1/3 lg:mb-0 p-4">
                             <div className="h-full text-center">
                                 <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://firebasestorage.googleapis.com/v0/b/devknus-official-database.appspot.com/o/images%2FScreenshot%202023-07-07%20at%202.20.32%20PM-modified.png?alt=media&token=324ddd80-2b40-422c-9f1c-1c1fa34943fa" />
-                                <p className="leading-relaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+                                <p className="leading-relaxed">Absolutely love their products! It exceeded my expectations in every way. The quality is top-notch, and shipping was incredibly fast.
+                                I’ve tried many similar products, but this one stands out! It’s durable, stylish, and worth every penny. Will definitely buy again</p>
                                 <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
                                 <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">XYZ </h2>
-                                <p className="text-gray-500">CTO</p>
+                                <p className="text-gray-500">Driver</p>
                             </div>
                         </div>
                     </div>
